41a1c8bd5a3b4d841be137c4bcee549736b49b98
ya-bystritskiy <bystritskii.yaroslav@gmail.com>
2024-06-12 12:07:34 +0300

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Ярослав Быстрицкий on 07.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
